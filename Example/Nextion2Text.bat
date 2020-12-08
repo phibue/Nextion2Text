e0d007ab5bc2e489b0e75e8796962c4171b74fe3
phibue <p.buehler@mailbox.org>
2020-12-08 19:30:30 +0100

    typo in help

--- Example/Nextion2Text.bat
+++ Example/Nextion2Text.bat
@@ -1,4 +1,4 @@
-python ..\Nextion2Text.py -d -p all -i"Number_Bug.HMI" -o"Number_Bug"
+python ..\Nextion2Text.py -d -p visual unknown -i"Number_Bug.HMI" -o"Number_Bug"
 
-rem python ..\Nextion2Text.py -h
+python ..\Nextion2Text.py -h
 pause
