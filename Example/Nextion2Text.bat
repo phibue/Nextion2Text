d2b94721f10e74294d4cc87dbcc96b93459842b1
phibue <p.buehler@mailbox.org>
2020-12-08 23:35:29 +0100

    Use original names

--- Example/Nextion2Text.bat
+++ Example/Nextion2Text.bat
@@ -1,4 +1,4 @@
-python ..\Nextion2Text.py -d -p visual unknown -i"Number_Bug.HMI" -o"Number_Bug"
+python ..\Nextion2Text.py -d -p visual unknown -i"Number_Bug.HMI" -o"Number_Bug" -n
 
 python ..\Nextion2Text.py -h
 pause
